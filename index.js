5495d65a4427fcfd55edfe5ba750174b6e25e075
Mitchell Gates <ms.fragala@gmail.com>
2016-05-22 18:26:58 -0400

    Refactor parsing strategy.

--- index.js
+++ index.js
@@ -1,63 +1,59 @@
 var HJSON = require('hjson');
+var escape = require('escape-string-regexp');
 
-module.exports = function(content, delimiter) {
-
-  content = removeBOM(content);
-  delimiter = delimiter || '---';
-
-  var lines = getLines(content);
-  var inceptor, terminus, frontMatter, fileContent;
-
-  if (lines[0] && isDelimiter(lines[0])) {
-    inceptor = getInceptor(content, delimiter);
-    terminus = getTerminus(content, delimiter, inceptor);
-    frontMatter = getFrontMatter(content, inceptor, terminus);
-    fileContent = getFileContent(content, inceptor, terminus);
-    return {
-      body: fileContent,
-      data: HJSON.parse(frontMatter),
-      original: content
-    }
-  } else {
-    return content;
+module.exports = function(input, options) {
+  /* normalize content */
+  var content = normalizeContent(input);
+  /* normalize delimiters */
+  var delimit = normalizeDelimit((options && options.delimiters));
+  /* set default result */
+  var result = { body: content, data: {}, original: content };
+  /* locate delimiter indexes */
+  var inceptor = getInceptor(content, delimit[0]);
+  var terminus = getTerminus(content, delimit[1]);
+  /* test for correct delimitation */
+  if (inceptor.start === 0 && terminus.start > 0) {
+    /* dissect content frontmatter */
+    var data = content.slice(inceptor.end, terminus.start).trim();
+    var body = content.slice(terminus.end).trimLeft();
+    result.data = attemptParse(data);
+    result.body = body;
   }
-
+  return result;
 }
 
-function getLines(string) {
-  return string.split(/\r?\n/);
+function normalizeContent(content) {
+  if (typeof content !== 'string') content = content.toString();
+  if (content.charAt(0) === '\uFEFF') content = content.slice(1);
+  return content;
 }
 
-function isDelimiter(line, delimiter) {
-  return new Regex('^' + delimiter + '$').test(line);
-}
-
-function removeBOM(string) {
-  return (string.charAt(0) === '\uFEFF')
-    ? string.slice(1)
-    : string;
+function normalizeDelimit(delimiters) {
+  if (!Array.isArray(delimiters)) delimiters = [(delimiters||'---')];
+  if (delimiters.length !== 2) delimiters[1] = delimiters[0];
+  return delimiters;
 }
 
 function getInceptor(content, delimiter) {
-  var start = content.indexOf(delimiter);
+  var start = content.search(new RegExp('^' + escape(delimiter) + '\n\r?'));
   return {
     start: start,
-    end: start + delimiter.length
+    end: start + delimiter.length + 1
   }
 }
 
-function getTerminus(content, delimiter, inceptor) {
-  var start = content.indexOf(delimiter, inceptor.end);
+function getTerminus(content, delimiter) {
+  var start = content.search(new RegExp('\n\r?' + escape(delimiter) + '\n\r?'));
   return {
     start: start,
-    end: start + delimiter.length
+    end: start + delimiter.length + 1
   }
 }
 
-function getFrontMatter(content, inceptor, terminus) {
-  return content.substring(inceptor.end, terminus.start).trim();
-}
-
-function getFileContent(content, inceptor, terminus) {
-  return content.substring(terminus.end, content.length).trim();
+function attemptParse(data) {
+  try {
+    return HJSON.parse(data);
+  } catch(err) {
+    return {};
+  }
 }
